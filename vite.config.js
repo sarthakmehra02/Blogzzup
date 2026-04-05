b58823cb56c752d22df900b0e8f3b0d951821c66
nmnroy <naman.05114815723@cseds.mait.ac.in>
2026-04-05 15:10:21 +0530

    fix(deploy): fix case sensitivity in vite base path to resolve blank github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Blogzzup/',
+  base: '/BlogzzUP/',
   plugins: [react()],
   optimizeDeps: {
     include: ['axios', 'showdown', 'turndown', 'lucide-react']
